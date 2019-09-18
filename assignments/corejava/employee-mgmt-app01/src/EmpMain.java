68ee87812697802240e953e2fe542a3abdd520bf
AdityaNair <AdityaNair@DESKTOP-AT6KPNL.in.ibm.com>
2019-09-18 08:23:52 +0530

    Second Commit Added import and export functions

--- assignments/corejava/employee-mgmt-app01/src/EmpMain.java
+++ assignments/corejava/employee-mgmt-app01/src/EmpMain.java
@@ -1,10 +1,11 @@
 import java.util.*;
 public class EmpMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in); 
-       Emp n=new Emp();
+		EmpServ Serv=new EmpServ();
+       
 		int select =0;
 		do
 		{
@@ -13,29 +14,51 @@ public class EmpMain {
 			System.out.println("2) ADD  EMPLOYEE");
 			System.out.println("3) VIEW EMPLOYEE");
 			System.out.println("4) REMOVE EMPLOYEE ");
-			System.out.println("5) EXIT ");
+			System.out.println("5) UPDATE EMPLOYEE");
+			System.out.println("6) IMPORT EMPLOYEE");
+			System.out.println("7) EXPORT EMPLOYEE");
+			System.out.println("8) EXIT ");
 			
 			select=sc.nextInt();
 			switch(select)
 			{
-				case 2:
-					n.addUser();
-					break;
 				case 1:
-					n.viewAll();
+					Serv.viewAll();
+					break;
+				case 2:
+					Serv.addUser();
+					
 					break;
 				case 3:
-					n.view();
+					Serv.view();
 					break;
 				case 4:
-					n.removeUser();
+					Serv.removeUser();
+					
+					break;
+				case 5:
+					
+				break;
+					
+				case 6:
+					Serv.importEmp();
+					
+					
+				break;
+				case 7:
+					Serv.exportEmp();
+					
+					break;
+				case 8:
+					
+					break;
 					
 					
 			}
 			
 			
 			
-		} while(select != 5);
+		} while(select != 8);
 		System.out.println("EXITING .....");
 
 	}
